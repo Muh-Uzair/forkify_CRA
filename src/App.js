f9e5d8c2c9030d1147fe557356a2df1a69437c1c
Muh-Uzair <muhammaduzair1062001@gmail.com>
2024-05-03 18:00:03 +0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+
+
+export default function App() {
+
+  return(
+    <div className="div_containing_everything">
+
+
+      <main className="main_box">
+
+        <HEADER_COMPONENT></HEADER_COMPONENT>
+
+
+        <section className="section_left_right_both"></section>
+
+      </main>
+
+
+
+
     </div>
-  );
+  )
 }
 
-export default App;
+
+function HEADER_COMPONENT() {
+
+  return(
+
+    <header className="section_header">
+
+      <div className="div_forkify_logo">
+        <img className="img_forkify_logo" src="forkify_logo.png" alt="img"/>
+      </div>
+
+
+      <div className="div_search_bar">
+         <form className="form_input_search_bar">
+
+
+           <input className="input_search_bar" placeholder="Search recipe"/>
+           
+         <button className="btn_search">
+                <img className="img_search_icon" src="search_icon_2.png"  alt="img"/>
+                <p className="text_search">SEARCH</p>
+           </button>
+
+          
+         </form>
+      </div>
+
+
+      <div className="div_add_recipes">
+         <img className="img_add_recipe_icon" src="add_recipe_icon_2.png" alt="img"/> 
+        <p className="text_add_recipe">ADD RECIPE</p>
+      </div>
+
+
+
+      <div className="div_add_bookmarks">
+        <img className="img_add_recipe_icon" src="add_book_mark_icon_4.png" alt="img"/> 
+        <p className="text_add_recipe">BOOKMARKS</p>
+      </div>
+
+      
+
+    </header>
+
+  )
+}
