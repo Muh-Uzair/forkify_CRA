7c05a7bdb3d9b469c857b2a5a52b7707cb0a4a63
Muh-Uzair <muhammaduzair1062001@gmail.com>
2024-05-06 09:02:11 +0500

    componenting done Alhamdulillah

---
+++ src/components_folder/HEADER_COMPONENT.js
@@ -0,0 +1,122 @@
+import { useRef, useState } from "react";
+import { API_KEY } from "../App";
+
+////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////
+export function HEADER_COMPONENT({
+  arr_of_recipes, set_arr_of_recipes, is_loading, set_is_loading, page_num, set_page_num, check_for_no_results, set_check_for_no_results,
+}) {
+
+
+  const [inputed_recipe_name, set_inputed_recipe_name] = useState("");
+  const check_for_same_search = useRef("");
+
+
+  //__________________________________________________________________________________________
+  function handle_form_submit_search_btn_click(event_info_object) {
+    event_info_object.preventDefault();
+
+    set_inputed_recipe_name("");
+
+    if (check_for_same_search.current === inputed_recipe_name) {
+
+      return;
+    }
+
+    check_for_same_search.current = inputed_recipe_name;
+
+
+    fetch_recipe();
+
+
+    set_page_num(1);
+
+  }
+  //__________________________________________________________________________________________
+  async function fetch_recipe() {
+
+
+
+    try {
+
+      set_is_loading(true);
+
+      const respose = await fetch(`https://forkify-api.herokuapp.com/api/v2/recipes?search=${inputed_recipe_name}&key=${API_KEY}`);
+      const data = await respose.json();
+
+      // const recieved_arr_of_reciepes = [...data.data.recipes] ;
+      set_arr_of_recipes(arr_of_recipes => [{}, ...data.data.recipes]);
+
+      set_is_loading(false);
+
+
+    }
+    catch (err) {
+
+      console.log(err);
+    }
+    finally {
+    }
+
+
+  }
+  //__________________________________________________________________________________________
+  function handle_input_change(event_info_object) {
+
+    set_inputed_recipe_name(event_info_object.target.value);
+
+  }
+
+
+
+
+
+
+  //---------------------------------------------------------------------------------
+  return (
+
+    <header className="section_header">
+
+      <div className="div_forkify_logo">
+        <img className="img_forkify_logo" src="forkify_logo.png" alt="img" />
+      </div>
+
+
+      <div className="div_search_bar">
+        <form className="form_input_search_bar"
+          onSubmit={(e) => handle_form_submit_search_btn_click(e)}>
+
+
+          <input className="input_search_bar" placeholder="Search recipe"
+            value={inputed_recipe_name}
+            onChange={(e) => handle_input_change(e)} />
+
+          <button className="btn_search" onClick={(e) => handle_form_submit_search_btn_click(e)}>
+            <img className="img_search_icon" src="search_icon_2.png" alt="img" />
+            <p className="text_search">SEARCH</p>
+          </button>
+
+
+        </form>
+      </div>
+
+
+      <div className="div_add_recipes">
+        <img className="img_add_recipe_icon" src="add_recipe_icon_2.png" alt="img" />
+        <p className="text_add_recipe">ADD RECIPE</p>
+      </div>
+
+
+
+      <div className="div_add_bookmarks">
+        <img className="img_add_recipe_icon" src="add_book_mark_icon_5.png" alt="img" />
+        <p className="text_add_recipe">BOOKMARKS</p>
+      </div>
+
+
+
+    </header>
+
+  );
+  //---------------------------------------------------------------------------------
+}
---
+++ src/components_folder/RIGHT_COMPONENT.js
@@ -0,0 +1,154 @@
+/////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////
+export function RIGHT_COMPONENT({
+  recipe_clicked, set_recipe_clicked, is_loading_right, set_is_loading_right, recipe_details, set_recipe_details, check_book_mark_right_clicked, set_check_book_mark_right_clicked, bookmarks_arr, set_bookmarks_arr,
+
+}) {
+
+
+  //__________________________________________________________________________________
+  function check_for_bookmarked_recipe_existance_function() {
+
+    let flag = false;
+
+    for (let i = 0; i < bookmarks_arr.length; i++) {
+      if (bookmarks_arr[i] === recipe_details.id) {
+        flag = true;
+      }
+    }
+
+    return flag;
+  }
+  //__________________________________________________________________________________
+  function book_mark_right_clicked_function(event_info_object) {
+
+
+    if (check_for_bookmarked_recipe_existance_function() === false) {
+      // console.log(`not exist : so added`)
+      set_bookmarks_arr(bookmarks_arr => [...bookmarks_arr, recipe_details.id]);
+      set_check_book_mark_right_clicked(true);
+    }
+    else if (check_for_bookmarked_recipe_existance_function() === true) {
+      // console.log(`exist : so removed`)
+      set_bookmarks_arr(bookmarks_arr => bookmarks_arr.filter(val => val !== recipe_details.id));
+      set_check_book_mark_right_clicked(false);
+    }
+
+  }
+
+
+
+  return (
+
+    <section className="section_right">
+
+      {!recipe_clicked &&
+        <div className="div_before_searching">
+          <img className="img_smiley_face" src="smiley_face.png" alt="img" />
+          <p className="text_start_searching">Start by searching for a recipe or <br></br> an ingredient. Have fun!</p>
+
+        </div>}
+      {is_loading_right ?
+        <p className="text_loading">LOADING...</p>
+        :
+        recipe_clicked
+          ?
+          <div className="div_show_recipe_detials">
+
+            <div className="div_img_big_recipe">
+              <img className="img_big_recipe" src={recipe_details.image_url} alt="img" />
+            </div>
+
+
+            <div className="div_time_plus_serving_plus_minus">
+
+              <div className="div_time">
+                <img className="clock_img" src="clock_icon.png" alt="img" />
+                <p className="text_time"><strong>{recipe_details.cooking_time}</strong> MINUTES</p>
+
+              </div>
+
+              <div className="div_servings">
+
+                <img className="img_servings" src="servings_icon.png" alt="img" />
+
+                <p className="text_servings"><strong>{recipe_details.servings}</strong> SERVINGS</p>
+
+                <button className="btn_plus">
+                  <img className="img_plus" src="plus_icon.png" alt="img" />
+                </button>
+
+                <button className="btn_minus">
+                  <img className="img_minus" src="minus_icon.png" alt="img" />
+                </button>
+
+
+              </div>
+
+              <div className="div_btn_bookmark_right">
+
+                <button className="btn_bookmark_right" onClick={(e) => book_mark_right_clicked_function(e)}>
+
+                  {check_book_mark_right_clicked ?
+                    <img className="img_bookmark_right" src="bookmark_icon_right.png" alt="img" />
+                    :
+                    <img className="img_bookmark_right" src="bookmark_icon_right_2.png" alt="img" />}
+                </button>
+
+              </div>
+
+            </div>
+
+
+            <div className="div_recipe_ingredients">
+
+              <p className="text_ingrients">RECIPE INGREDIENTS</p>
+
+              <div></div>
+
+              <div className="div_all_recipe_ingredients">
+                <ul>
+                  {recipe_details.ingredients.slice(0, Math.trunc(recipe_details.ingredients.length) / 2)
+                    .map((val, i) => (
+                      <li key={i}>
+                        <img className="img_tick_icon" src="tick_icon_2.png" alt="img" />
+                        <p className="text_recipe_ingredients_detailed">{val.description}</p>
+                      </li>
+                    ))}
+                </ul>
+              </div>
+
+              <div className="div_all_recipe_ingredients">
+                <ul>
+                  {recipe_details.ingredients.slice((Math.trunc(recipe_details.ingredients.length) / 2), Math.trunc(recipe_details.ingredients.length))
+                    .map((val, i) => (
+                      <li key={i}>
+                        <img className="img_tick_icon" src="tick_icon_2.png" alt="img" />
+                        <p className="text_recipe_ingredients_detailed">{val.quantity} {val.unit} {val.description}</p>
+
+                      </li>
+                    ))}
+                </ul>
+              </div>
+
+            </div>
+
+            <div className="div_how_to_cook_it">
+
+              <p className="text_how_to_cook_it">HOW TO COOK IT</p>
+              <p className="text_carefully">This recipe was carefully designed and tested by All Recipes. Please check out<br />directions at their website.</p>
+
+              <a href={recipe_details.source_url} target="_blank" rel="noreferrer">
+                <button className="btn_directions">DIRECTIONS &rarr;</button>
+              </a>
+
+            </div>
+
+
+          </div>
+          :
+          <></>}
+
+    </section>
+  );
+}
